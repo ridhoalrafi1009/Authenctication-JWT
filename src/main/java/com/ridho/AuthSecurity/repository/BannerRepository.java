3d4f8fd737394a1ca6038f8d6b8bcd319652f952
ridhoalrafi1009 <rihdoalrafi@gmail.com>
2023-11-04 16:09:01 +0700

    add properties application and .env

--- src/main/java/com/ridho/AuthSecurity/repository/BannerRepository.java
+++ src/main/java/com/ridho/AuthSecurity/repository/BannerRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.ridho.AuthSecurity.models.Banner;
 import com.ridho.AuthSecurity.models.response.BannerResponse;
 
 @Repository
